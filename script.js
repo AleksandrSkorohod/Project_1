294c0c25b898998900df0a6d3daf516f14ce28bd
Sashka <alex198623@mail.ru>
2022-12-23 12:24:52 +0300

    свойства и  методы строк и чисел

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
 
 
-
-function showText(text) {
-    console.log(text);
-    let num = 20;
-}
-
-showText('привет сабака');
-console.log(num);
-
-function calc(a, b) {
-    return (a + b);
-}
-
-console.log(calc(12, 18));
-console.log(calc(14, 158));
-console.log(calc(1254, 1228));
+// const text = 'падла дурны, сабака пъяная';
+const arr = [2, 4, 2, 7]
 
 
-function net () {
-    num = 35;
+console.log(text.length);    // обращение к свойству, через точку, без скобок
+console.log(text[2]);        // можно обратиться к строке по индексу
 
-    //какие то вычисления
-    //многа многа вычислений..
-    return num;
-}
 
-const newNum = net();
-console.log(newNum);
+console.log(text.indexOf('са'));   // с какого индекса начинается кусок строки
+console.log(text);
 
-const logger = function() {
-    console.log('hello');
-};
 
-logger();
+const text = 'падла дурны, сабака пъяная';
 
-const strFun = (a, b) => {
-    console.log('go to hell');
-    return a + b;
-}
 
-console.log(strFun(12, 34));
+console.log(text.slice(-6));    // срез, по индексу.
+console.log(text);
+
+console.log(text.substring(6, 11));   //неподдерживает отрицательные значения индексов
+
+const num = 12.3;
+
+console.log(Math.round(num));   //округление
+
+const test = '15.65px';
+
+console.log(parseInt(test));   //строку в числовой в целое число
+console.log(parseFloat(test));    //строку в числовой в дробное
